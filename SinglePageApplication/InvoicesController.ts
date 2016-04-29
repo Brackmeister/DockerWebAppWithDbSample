0218b86ef3182cf4dd6ba03331095992799af5d9
Brackmeister <ingo.strauch@gmx.net>
2016-04-29 17:59:12 +0200

    Added SPA project. Still in development (doesn't run yet)

---
+++ SinglePageApplication/InvoicesController.ts
@@ -0,0 +1,72 @@
+/// <reference path='angular.d.ts' />  
+/// <reference path='model.ts' />
+
+module Invoices {
+    export interface Scope {
+        newInvoiceId: number;
+        newInvoiceDescription: string;
+        newInvoiceCustomer: string;
+        Invoices: Model.Invoice[];
+        addNewInvoice: Function;
+    }
+
+    export class Controller {
+        private baseUrl: string;
+        private httpService: any;
+        constructor($scope: Scope, $http: any) {
+            this.baseUrl = "http://192.168.178.24:8080";
+            
+            this.httpService = $http;
+
+            this.refreshInvoices($scope);
+
+            var controller = this;
+
+            $scope.addNewInvoice = function () {
+                var newInvoice = new Model.Invoice();
+                newInvoice.Id =$scope.newInvoiceId;
+                newInvoice.Description = $scope.newInvoiceDescription;
+                newInvoice.Customer = $scope.newInvoiceCustomer;
+
+                controller.addInvoice(newInvoice, function () {
+                    controller.getAllInvoices(function (data) {
+                        $scope.Invoices = data;
+                    },function(error){
+                        alert(error);
+                    });
+                });
+            };
+
+        }
+
+        getAllInvoices(successCallback: Function, errorCallback: Function): void {
+            this.httpService.get(this.baseUrl+'/api/invoices')
+                .success(function (data, status) {
+                    successCallback(data);
+                })
+                .error(function (data, status) {
+                    errorCallback(status);
+                });
+        }
+
+        addInvoice(Invoice: Model.Invoice, successCallback: Function): void {
+            this.httpService.post(this.baseUrl+'/api/invoices', Invoice).success(function () {
+                successCallback();
+            });
+        }
+
+        deleteInvoice(InvoiceId: string, successCallback: Function): void {
+            this.httpService.delete(this.baseUrl+'/api/Invoices/' + InvoiceId).success(function () {
+                successCallback();
+            });
+        }
+
+        refreshInvoices(scope: Scope) {
+            this.getAllInvoices(function (data) {
+                scope.Invoices = data;
+            },function(error){
+               alert(error);
+            });
+        }
+    }
+}
---
+++ SinglePageApplication/Model.ts
@@ -0,0 +1,7 @@
+module Model {  
+    export class Invoice {
+        Id: number;
+        Description: string;
+        Customer: string;
+    }
+}
